bfd1727449b02bf118b657b92ed920c4ff117c08
李二庆（Amos Lee） <iwillhappy1314@gmail.com>
2019-05-10 19:02:48 +0800

    测试自动部署

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,99 +1,82 @@
 #!/usr/bin/env bash
 
-if [[ -z "$TRAVIS" ]]; then
-	echo "Script is only to be run by Travis CI" 1>&2
-	exit 1
+set -e
+
+if [[ "false" != "$TRAVIS_PULL_REQUEST" ]]; then
+	echo "Not deploying pull requests."
+	exit
 fi
 
-if [[ -z "$WP_ORG_PASSWORD" ]]; then
-	echo "WordPress.org password not set" 1>&2
-	exit 1
+if [[ ! $WP_PULUGIN_DEPLOY ]]; then
+	echo "Not deploying."
+	exit
 fi
 
-if [[ -z "$TRAVIS_BRANCH" || "$TRAVIS_BRANCH" != "master" ]]; then
-	echo "Build branch is required and must be 'master'" 1>&2
-	exit 0
+if [[ ! $SVN_REPO ]]; then
+	echo "SVN repo is not specified."
+	exit
 fi
 
-WP_ORG_USERNAME="iwillhappy1314"
-PLUGIN="wenprise-term-group"
-PROJECT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
-PLUGIN_BUILDS_PATH="$PROJECT_ROOT/builds"
-#VERSION=$(/usr/bin/php -f "$PLUGIN_BUILD_CONFIG_PATH/utils/get_plugin_version.php" "$PROJECT_ROOT" "$PLUGIN")
-VERSION="1.0.3"
-ZIP_FILE="$PLUGIN_BUILDS_PATH/$PLUGIN-$VERSION.zip"
+# Untrailing slash of SVN_REPO path
+SVN_REPO=`echo $SVN_REPO | sed -e "s/\/$//"`
+# Git repository
+GH_REF=https://github.com/${TRAVIS_REPO_SLUG}.git
 
-# Ensure the zip file for the current version has been built
-if [ ! -f "$ZIP_FILE" ]; then
-    echo "Built zip file $ZIP_FILE does not exist" 1>&2
-    exit 1
+echo "Starting deploy..."
+
+mkdir build
+
+cd build
+BASE_DIR=$(pwd)
+
+echo "Checking out trunk from $SVN_REPO ..."
+svn co -q $SVN_REPO/trunk
+
+echo "Getting clone from $GH_REF to $SVN_REPO ..."
+git clone -q $GH_REF ./git
+
+cd ./git
+
+if [ -e "bin/build.sh" ]; then
+	echo "Starting bin/build.sh."
+	bash bin/build.sh
 fi
 
-# Check if the tag exists for the version we are building
-TAG=$(svn ls "https://plugins.svn.wordpress.org/$PLUGIN/tags/$VERSION")
-error=$?
-if [ $error == 0 ]; then
-    # Tag exists, don't deploy
-    echo "Tag already exists for version $VERSION, aborting deployment"
-    exit 1
+cd $BASE_DIR
+
+echo "Syncing git repository to svn"
+rsync -a --exclude=".svn" --checksum --delete ./git/ ./trunk/
+rm -fr ./git
+
+cd ./trunk
+
+if [ -e ".distignore" ]; then
+	echo "svn propset form .distignore"
+	svn propset -q -R svn:ignore -F .distignore .
+
+else
+	if [ -e ".svnignore" ]; then
+		echo "svn propset"
+		svn propset -q -R svn:ignore -F .svnignore .
+	fi
 fi
 
-cd "$PLUGIN_BUILDS_PATH"
-# Remove any unzipped dir so we start from scratch
-rm -fR "$PLUGIN"
-# Unzip the built plugin
-unzip -q -o "$ZIP_FILE"
+echo "Run svn add"
+svn st | grep '^!' | sed -e 's/\![ ]*/svn del -q /g' | sh
+echo "Run svn del"
+svn st | grep '^?' | sed -e 's/\?[ ]*/svn add -q /g' | sh
 
-# Clean up any previous svn dir
-rm -fR svn
-
-# Checkout the SVN repo
-svn co -q "http://svn.wp-plugins.org/$PLUGIN" svn
-
-# Move out the trunk directory to a temp location
-mv svn/trunk ./svn-trunk
-# Create trunk directory
-mkdir svn/trunk
-# Copy our new version of the plugin into trunk
-rsync -r -p $PLUGIN/* svn/trunk
-
-# Copy all the .svn folders from the checked out copy of trunk to the new trunk.
-# This is necessary as the Travis container runs Subversion 1.6 which has .svn dirs in every sub dir
-cd svn/trunk/
-TARGET=$(pwd)
-cd ../../svn-trunk/
-
-# Find all .svn dirs in sub dirs
-SVN_DIRS=`find . -type d -iname .svn`
-
-for SVN_DIR in $SVN_DIRS; do
-    SOURCE_DIR=${SVN_DIR/.}
-    TARGET_DIR=$TARGET${SOURCE_DIR/.svn}
-    TARGET_SVN_DIR=$TARGET${SVN_DIR/.}
-    if [ -d "$TARGET_DIR" ]; then
-        # Copy the .svn directory to trunk dir
-        cp -r $SVN_DIR $TARGET_SVN_DIR
-    fi
-done
-
-# Back to builds dir
-cd ../
-
-# Remove checked out dir
-rm -fR svn-trunk
-
-# Add new version tag
-mkdir svn/tags/$VERSION
-rsync -r -p $PLUGIN/* svn/tags/$VERSION
-
-# Add new files to SVN
-svn stat svn | grep '^?' | awk '{print $2}' | xargs -I x svn add x@
-# Remove deleted files from SVN
-svn stat svn | grep '^!' | awk '{print $2}' | xargs -I x svn rm --force x@
-svn stat svn
-
-# Commit to SVN
-svn ci --no-auth-cache --username $WP_ORG_USERNAME --password $WP_ORG_PASSWORD svn -m "Deploy version $VERSION"
-
-# Remove SVN temp dir
-rm -fR svn
+# If tag number and credentials are provided, commit to trunk.
+if [[ $TRAVIS_TAG && $SVN_USER && $SVN_PASS ]]; then
+	if [[ ! -d tags/$TRAVIS_TAG ]]; then
+		echo "Commit to $SVN_REPO."
+		svn commit -m "commit version $TRAVIS_TAG" --username $SVN_USER --password $SVN_PASS --non-interactive 2>/dev/null
+		echo "Take snapshot of $TRAVIS_TAG"
+		svn copy $SVN_REPO/trunk $SVN_REPO/tags/$TRAVIS_TAG -m "Take snapshot of $TRAVIS_TAG" --username $SVN_USER --password $SVN_PASS --non-interactive 2>/dev/null
+	else
+		echo "tags/$TRAVIS_TAG already exists."
+	fi
+else
+	echo "Nothing to commit and check \`svn st\`."
+	svn st
+fi

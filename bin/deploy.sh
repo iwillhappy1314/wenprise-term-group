12307c6ef8f7e2e794811c5e66e86b7500099a50
李二庆（Amos Lee） <iwillhappy1314@gmail.com>
2019-05-11 22:57:41 +0800

    测试自动部署

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -48,68 +48,49 @@ BASE_DIR=$(pwd)
 
 # 检出 SVN
 echo "Checking out trunk from $SVN_REPO ..."
-svn co -q $SVN_REPO/trunk
+svn co -q $SVN_REPO ./svn
 
 # 检出 Git，已经有了，是不是不需要再来一遍了，或者直接 checkout?
 echo "Getting clone from $GH_REF to $SVN_REPO ..."
 git clone -q $GH_REF ./git
 
-echo "基本目录 000";
-ls -la
-
-
-echo "trunk 目录 000";
-ls $BASE_DIR/trunk -la
-
 # 如果设置了构建脚本，开始构建
-cd ./git
+cd $BASE_DIR/git
 
 if [ -e "bin/build.sh" ]; then
 	echo "Starting bin/build.sh."
 	bash bin/build.sh
 fi
 
+#####################################################
+# 同步文件
+#####################################################
 # 同步 git 仓库到 SVN
 cd $BASE_DIR
 echo "Syncing git repository to svn"
-rsync -a --exclude=".svn" --checksum --delete ./git/ ./trunk/
+rsync -a --exclude=".svn" --checksum --delete ./git/ ./svn/trunk/
 
-rm $BASE_DIR/git -Rf
-
-
-echo "trunk 目录";
-ls $BASE_DIR/trunk -la
-
-echo "同步后的目录";
-ls -la
-
+rm $BASE_DIR/.git -Rf
 
 #####################################################
 # 比较版本，如果两个版本不一样，退出
 #####################################################
-# 检查 readme.txt 中的版本和插件主文件中的版本
-READMEVERSION=`grep "Stable tag" $BASE_DIR/trunk/readme.txt | awk '{ print $NF}'`
-PLUGINVERSION=`grep "Version:" $BASE_DIR/trunk/$MAINFILE | awk '{ print $NF}'`
+READMEVERSION=`grep "Stable tag" $BASE_DIR/svn/trunk/readme.txt | awk '{ print $NF}'`
+PLUGINVERSION=`grep "Version:" $BASE_DIR/svn/trunk/$MAINFILE | awk '{ print $NF}'`
 
-#if [ "$READMEVERSION" != "$PLUGINVERSION" ]; then
-#    echo "Versions don't match. Exiting....";
-#    exit 1
-#fi
+if [ "$READMEVERSION" != "$PLUGINVERSION" ]; then
+    echo "Versions don't match. Exiting....";
+    exit 1
+fi
 
 #####################################################
-# 忽略文件
+# 设置忽略文件
 #####################################################
-cd ./trunk
+cd $BASE_DIR/svn/trunk
 
-if [ -e ".distignore" ]; then
-	echo "svn propset form .distignore"
-	svn propset -q -R svn:ignore -F .distignore .
-
-else
-	if [ -e ".svnignore" ]; then
-		echo "svn propset"
-		svn propset -q -R svn:ignore -F .svnignore .
-	fi
+if [ -e ".svnignore" ]; then
+    echo "svn propset form .svnignore"
+    svn propset -q -R svn:ignore -F .svnignore .
 fi
 
 
@@ -125,23 +106,14 @@ svn st | grep '^?' | sed -e 's/\?[ ]*/svn add -q /g' | sh
 #####################################################
 # 如果设置了用户名密码，提交到仓库，必须是 Tag 才能提交
 #####################################################
-echo $(pwd)
+echo "svn 目录";
+ls $BASE_DIR/svn -la
 
-echo "部署目录";
-ls -la
+echo "svn trunk 目录";
+ls $BASE_DIR/svn/trunk -la
 
-#cd $BASE_DIR
-#
-#echo $(pwd)
+svn copy $BASE_DIR/svn/trunk/ $BASE_DIR/svn/tags/$READMEVERSION/
 
-cd $BASE_DIR
-
-echo "基本目录";
-ls -la
-#svn copy ./trunk/ tags/$READMEVERSION/
-
-cd $BASE_DIR/trunk
-
+cd $BASE_DIR/svn
 svn stat
-
 svn ci --no-auth-cache --username $WP_ORG_USERNAME --password $WP_ORG_PASSWORD -m "Deploy version $READMEVERSION"

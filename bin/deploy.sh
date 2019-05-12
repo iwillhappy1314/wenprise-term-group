f0527e7ba2ddaffaeedeaa8c490800751048c2a9
李二庆（Amos Lee） <iwillhappy1314@gmail.com>
2019-05-12 09:24:17 +0800

    测试忽略文件

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-# args
+#####################################################
+# 定义变量，需要根据插件和用户修改
+#####################################################
 MAINFILE="wenprise-term-group.php"
 WP_ORG_USERNAME="iwillhappy1314"
-
-#####################################################
-# 定义 Git 和 SVN 仓库
-#####################################################
 SVN_REPO="https://plugins.svn.wordpress.org/wenprise-term-group/"
-GH_REF=https://github.com/${TRAVIS_REPO_SLUG}.git
+GIT_REPO=https://github.com/${TRAVIS_REPO_SLUG}.git
 
 
 #####################################################
@@ -42,18 +40,18 @@ echo "Starting deploy..."
 mkdir build
 
 cd build
-BASE_DIR=$(pwd)
+BUILT_DIR=$(pwd)
 
 # 检出 SVN
 echo "Checking out trunk from $SVN_REPO ..."
 svn co -q $SVN_REPO ./svn
 
 # 检出 Git，已经有了，是不是不需要再来一遍了，或者直接 checkout?
-echo "Getting clone from $GH_REF to $SVN_REPO ..."
-git clone -q $GH_REF ./git
+echo "Getting clone from $GIT_REPO to $SVN_REPO ..."
+git clone -q $GIT_REPO ./git
 
 # 如果设置了构建脚本，开始构建
-cd $BASE_DIR/git
+cd $BUILT_DIR/git
 
 if [ -e "bin/build.sh" ]; then
 	echo "Starting bin/build.sh."
@@ -64,17 +62,17 @@ fi
 # 同步文件
 #####################################################
 # 同步 git 仓库到 SVN
-cd $BASE_DIR
+cd $BUILT_DIR
 echo "Syncing git repository to svn"
 rsync -a --exclude=".svn" --checksum --delete ./git/ ./svn/trunk/
 
-rm $BASE_DIR/svn/trunk/.git -Rf
+rm $BUILT_DIR/svn/trunk/.git -Rf
 
 #####################################################
 # 比较版本，如果两个版本不一样，退出
 #####################################################
-READMEVERSION=`grep "Stable tag" $BASE_DIR/svn/trunk/readme.txt | awk '{ print $NF}'`
-PLUGINVERSION=`grep "Version:" $BASE_DIR/svn/trunk/$MAINFILE | awk '{ print $NF}'`
+READMEVERSION=`grep "Stable tag" $BUILT_DIR/svn/trunk/readme.txt | awk '{ print $NF}'`
+PLUGINVERSION=`grep "Version:" $BUILT_DIR/svn/trunk/$MAINFILE | awk '{ print $NF}'`
 
 if [ "$READMEVERSION" != "$PLUGINVERSION" ]; then
     echo "Versions don't match. Exiting....";
@@ -82,10 +80,11 @@ if [ "$READMEVERSION" != "$PLUGINVERSION" ]; then
 fi
 
 #####################################################
-# 设置忽略文件
+# 设置忽略文件、删除忽略的文件
 #####################################################
-cd $BASE_DIR/svn/trunk
+cd $BUILT_DIR/svn/trunk
 
+# 设置 svn 忽略
 if [ -e ".svnignore" ]; then
     echo "svn propset form .svnignore"
     svn propset -q -R svn:ignore -F .svnignore
@@ -111,13 +110,19 @@ svn st | grep '^?' | sed -e 's/\?[ ]*/svn add -q /g' | sh
 # 如果设置了用户名密码，提交到仓库，必须是 Tag 才能提交
 #####################################################
 echo "svn 目录";
-ls $BASE_DIR/svn -la
+ls $BUILT_DIR/svn -la
 
 echo "svn trunk 目录";
-ls $BASE_DIR/svn/trunk -la
+ls $BUILT_DIR/svn/trunk -la
 
-svn copy $BASE_DIR/svn/trunk/ $BASE_DIR/svn/tags/$READMEVERSION/
+# 复制文件到 tag，如果 Tag 不存在，跳过
+TAG=$(svn ls "SVN_REPO/tags/$READMEVERSION")
+error=$?
+if [ $error != 0 ]; then
+    svn copy $BUILT_DIR/svn/trunk/ $BUILT_DIR/svn/tags/$READMEVERSION/
+    exit 1
+fi
 
-cd $BASE_DIR/svn
+cd $BUILT_DIR/svn
 svn stat
 svn ci --no-auth-cache --username $WP_ORG_USERNAME --password $WP_ORG_PASSWORD -m "Deploy version $READMEVERSION"
